205c09362f7e6c353b4ca656598a2a94c832ce64
Prass <Prasith.somchit@gmail.com>
2016-10-17 12:04:24 +0200

    made some changes

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var previaApp = angular.module('previaApp',[]);
 
 previaApp.controller('myCtrl', ['$scope', function($scope) {
 
-$scope.buttonToggle = function(buttonNumber){
+	$scope.buttonToggle = function(buttonNumber){
 
-this.pickChosen = buttonNumber === this.pickChosen? 0:buttonNumber;
+		this.pickChosen = buttonNumber === this.pickChosen? 0:buttonNumber;
 
 }
   }]);
