0c56aed6baeb12573b0f867957fa99d489e87206
Prass <Prasith.somchit@gmail.com>
2016-11-08 15:28:55 +0100

    made some new things

--- components/contact/contactCTRL.js
+++ components/contact/contactCTRL.js
@@ -3,26 +3,24 @@ previaApp.controller('contactCTRL', function($scope, $location) {
 
     $scope.myPath = $location.path();
 
-   	$scope.contact = "Contact us here from " + $scope.myPath;
+   	//$scope.contact = "Contact us here from " + $scope.myPath;
 
 
-/*
-   	 function(){
+   	$scope.contact = function(){
 
 	    if ($scope.myPath == '/home'){
 
-	    	"Contact us here in home"
+	    	$scope.contact = "Contact us here in home";
 	    }
 
 	    else if ($scope.myPath == '/login'){
 
-	    	"Contact us here in login"
+	    	$scope.contact = "Contact us here in login";
 	    }
 	}
 
-	    $location.hash = "home";
+	$scope.contact();
 
-*/
 	console.log($scope.contact);
 	console.log($scope.myPath);
 
